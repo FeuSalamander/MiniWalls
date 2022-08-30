18d43f93f914ea576b6fc77412bfac04685d9a92
FeuSalamander <ferreresarnaud@gmail.com>
2022-08-30 20:11:56 +0200

    leave item,auto team

--- src/main/java/me/feusalamander/miniwalls/listeners/listener.java
+++ src/main/java/me/feusalamander/miniwalls/listeners/listener.java
@@ -58,10 +58,15 @@ public class listener implements Listener {
                 gwool.setItemMeta(woolg);
                 player.getInventory().setItem(7, gwool);
                 ItemStack ywool = new ItemStack(Material.YELLOW_WOOL);
-                ItemMeta wooly = bwool.getItemMeta();
+                ItemMeta wooly = ywool.getItemMeta();
                 wooly.setDisplayName("§6Click to join the §eYellow Team !");
                 ywool.setItemMeta(wooly);
                 player.getInventory().setItem(8, ywool);
+                ItemStack leave = new ItemStack(Material.IRON_TRAPDOOR);
+                ItemMeta leave2 = leave.getItemMeta();
+                leave2.setDisplayName("§cClick to leave");
+                leave.setItemMeta(leave2);
+                player.getInventory().setItem(0, leave);
                 e.getPlayer().setScoreboard(main.scoreboard);
                 main.scoreboard.getTeam("playerss").setSuffix("§a" +main.getPlayers().size()+ "/§a8");
                 main.scoreboard.getTeam("playerss").setPrefix("Waiting ");
@@ -158,35 +163,32 @@ public class listener implements Listener {
         if(main.getPlayers().contains(player)){
             if(player.getItemInHand().getType() == Material.BLUE_WOOL){
                 if(player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("§1Click to join the §9Blue Team !")){
-                    if(main.scoreboard.getTeam("Yellow").getSize() < 2){
+                    if(main.scoreboard.getTeam("Blue").getSize() < 2){
                         main.scoreboard.getTeam("Blue").addPlayer(player);
                         player.sendMessage("§9You joined the Blue Team");
                     }else{
                         player.sendMessage("This team is full");
                     }
                 }
-            }
-            if(player.getItemInHand().getType() == Material.RED_WOOL){
+            }else if(player.getItemInHand().getType() == Material.RED_WOOL){
                 if(player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("§4Click to join the §cRed Team !")){
-                    if(main.scoreboard.getTeam("Yellow").getSize() < 2){
+                    if(main.scoreboard.getTeam("Red").getSize() < 2){
                         main.scoreboard.getTeam("Red").addPlayer(player);
                         player.sendMessage("§cYou joined the Red Team");
                     }else{
                         player.sendMessage("This team is full");
                     }
                 }
-            }
-            if(player.getItemInHand().getType() == Material.GREEN_WOOL){
+            }else if(player.getItemInHand().getType() == Material.GREEN_WOOL){
                 if(player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("§2Click to join the §aGreen Team !")){
-                    if(main.scoreboard.getTeam("Yellow").getSize() < 2){
+                    if(main.scoreboard.getTeam("Green").getSize() < 2){
                         main.scoreboard.getTeam("Green").addPlayer(player);
                         player.sendMessage("§aYou joined the Green Team");
                     }else{
                         player.sendMessage("This team is full");
                     }
                 }
-            }
-            if(player.getItemInHand().getType() == Material.YELLOW_WOOL){
+            }else if(player.getItemInHand().getType() == Material.YELLOW_WOOL){
                 if(player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("§6Click to join the §eYellow Team !")){
                     if(main.scoreboard.getTeam("Yellow").getSize() < 2){
                         main.scoreboard.getTeam("Yellow").addPlayer(player);
@@ -195,7 +197,36 @@ public class listener implements Listener {
                         player.sendMessage("This team is full");
                     }
                 }
+            }else if(player.getItemInHand().getType() == Material.IRON_TRAPDOOR){
+                if(player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("§cClick to leave")){
+                    Location lobby  = new Location(Bukkit.getWorld("world"), main.getConfig().getInt("Locations.Lobby.x"), main.getConfig().getInt("Locations.Lobby.y"), main.getConfig().getInt("Locations.Lobby.z"));
+                    player.teleport(lobby);
+                    player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+                    if(main.getPlayers().contains(player)){
+                        player.teleport(lobby);
+                        main.scoreboard.getTeam("Blue").removePlayer(player);
+                        main.scoreboard.getTeam("Red").removePlayer(player);
+                        main.scoreboard.getTeam("Green").removePlayer(player);
+                        main.scoreboard.getTeam("Yellow").removePlayer(player);
+                        if(main.isState(MWstates.WAITING)){
+                            main.getPlayers().remove(player);
+                            Bukkit.broadcastMessage(player.getName()+"§a left the MiniWalls game ! §7<§f" +main.getPlayers().size()+"§7/§f8§7>");
+                            player.setLevel(0);
+                            player.getInventory().clear();
+                        }else if(main.isState(MWstates.STARTING)){
+                            main.getPlayers().remove(player);
+                            Bukkit.broadcastMessage(player.getName()+"§a left the MiniWalls game ! §7<§f" +main.getPlayers().size()+"§7/§f8§7>");
+                            player.setLevel(0);
+                            player.getInventory().clear();
+                        }else{
+                            main.eliminate(player);
+                            for(Player list : main.getPlayers()) {
+                                list.sendMessage("§e"+player.getName()+"§eleft the game MiniWalls");
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-    }
 }
