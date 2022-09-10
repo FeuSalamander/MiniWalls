994f1b269d3736d1129bc5a09de33eb9e58a39bd
FeuSalamander <ferreresarnaud@gmail.com>
2022-09-10 09:16:13 +0200

    villager hit cd

--- src/main/java/me/feusalamander/miniwalls/listeners/villagerdmg.java
+++ src/main/java/me/feusalamander/miniwalls/listeners/villagerdmg.java
@@ -1,6 +1,8 @@
 package me.feusalamander.miniwalls.listeners;
 
 import me.feusalamander.miniwalls.MiniWalls;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
@@ -10,20 +12,27 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class villagerdmg implements Listener {
+    private boolean bc = true;
+    private boolean rc = true;
+    private boolean gc = true;
+    private boolean yc = true;
     private MiniWalls main;
     public villagerdmg(MiniWalls main) {
         this.main = main;
     }
-        @EventHandler
-        public void ondmg(EntityDamageByEntityEvent e){
-            Entity victim = e.getEntity();
-            Entity damager = e.getDamager();
-            if(damager instanceof Player){
-                Player player = (Player)damager;
-                if(main.getPlayers().contains(player)){
-                    if(victim.getName().equalsIgnoreCase("§9Blue Villager")){
+    @EventHandler
+    public void ondmg(EntityDamageByEntityEvent e){
+        Entity victim = e.getEntity();
+        Entity damager = e.getDamager();
+        if(damager instanceof Player){
+            Player player = (Player)damager;
+            if(main.getPlayers().contains(player)){
+                if(victim.getName().equalsIgnoreCase("§9Blue Villager")){
+                    if(bc){
                         if(!main.scoreboard.getTeam("Blue").hasPlayer(player)){
                             main.blife--;
+                            bc = false;
+                            Bukkit.getScheduler().runTaskLater(main, () -> bc = true, 30);
                             if(player.getHealth() > 4){
                                 player.damage(3);
                             }
@@ -43,9 +52,15 @@ public class villagerdmg implements Listener {
                         }else{
                             e.setCancelled(true);
                         }
-                    }else if(victim.getName().equalsIgnoreCase("§cRed Villager")){
+                    }else{
+                        e.setCancelled(true);
+                    }
+                }else if(victim.getName().equalsIgnoreCase("§cRed Villager")){
+                    if(rc){
                         if(!main.scoreboard.getTeam("Red").hasPlayer(player)){
                             main.rlife--;
+                            rc = false;
+                            Bukkit.getScheduler().runTaskLater(main, () -> rc = true, 30);
                             if(player.getHealth() > 4){
                                 player.damage(3);
                             }
@@ -65,9 +80,15 @@ public class villagerdmg implements Listener {
                         }else{
                             e.setCancelled(true);
                         }
-                    }else if(victim.getName().equalsIgnoreCase("§aGreen Villager")){
+                    }else{
+                        e.setCancelled(true);
+                    }
+                }else if(victim.getName().equalsIgnoreCase("§aGreen Villager")){
+                    if(gc){
                         if(!main.scoreboard.getTeam("Green").hasPlayer(player)){
                             main.glife--;
+                            gc = false;
+                            Bukkit.getScheduler().runTaskLater(main, () -> gc = true, 30);
                             if(player.getHealth() > 4){
                                 player.damage(3);
                             }
@@ -87,9 +108,15 @@ public class villagerdmg implements Listener {
                         }else{
                             e.setCancelled(true);
                         }
-                    }else if(victim.getName().equalsIgnoreCase("§eYellow Villager")){
+                    }else{
+                        e.setCancelled(true);
+                    }
+                }else if(victim.getName().equalsIgnoreCase("§eYellow Villager")){
+                    if(yc){
                         if(!main.scoreboard.getTeam("Yellow").hasPlayer(player)){
                             main.ylife--;
+                            yc = false;
+                            Bukkit.getScheduler().runTaskLater(main, () -> yc = true, 30);
                             if(player.getHealth() > 4){
                                 player.damage(3);
                             }
@@ -109,8 +136,11 @@ public class villagerdmg implements Listener {
                         }else{
                             e.setCancelled(true);
                         }
+                    }else{
+                        e.setCancelled(true);
                     }
                 }
             }
+        }
     }
 }
